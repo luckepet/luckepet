b0ddb3c428c2b12513f99022ac74a5d3b9378730
LuckePet <luckepet1@gmail.com>
2026-08-26 20:01:06 -0300

    Mejoras LuckePet carrito y WhatsApp

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,36 +1,43 @@
-
 import logo from '../assets/luckepetlogo.jpg'
-import { ShoppingCart } from "lucide-react";
+
 function Header({ 
-  setMostrarCarrito, 
+  setMostrarCarrito,
   cantidadCarrito,
   busqueda,
   setBusqueda
 }: any) {
   return (
     <header className="hero">
+
       <div className="topbar">
-        <img src={logo} alt="LuckePet" className="logo-img" />
 
-       <input
-  type="text"
-  placeholder="Buscar productos..."
-  value={busqueda}
-  onChange={(e) => setBusqueda(e.target.value)}
-/>
+        <img
+          src={logo}
+          alt="LuckePet"
+          className="logo-img"
+        />
 
-<button 
-  className="boton-carrito"
-  onClick={() => setMostrarCarrito(true)}
->
-  <ShoppingCart size={28} strokeWidth={3} color="black" />
-<span className="numero-carrito">{cantidadCarrito}</span>
-      </button>
+        <input
+          type="text"
+          placeholder="Buscar productos..."
+          value={busqueda}
+          onChange={(e) => setBusqueda(e.target.value)}
+        />
+
+        <button
+          className="boton-carrito"
+          onClick={() => setMostrarCarrito(true)}
+        >
+          <span style={{ fontSize: "28px" }}>
+            🛒
+          </span>
+
+          <span className="numero-carrito">
+            {cantidadCarrito}
+          </span>
+        </button>
 
       </div>
-  
-
-
 
     </header>
   )
